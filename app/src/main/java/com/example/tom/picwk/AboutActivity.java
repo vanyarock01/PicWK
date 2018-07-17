a1425e1d99c14533c0068e5d044efc7abd37af0f
vanyarock01 <vanyarock01@gmail.com>
2018-07-17 20:00:11 +0300

    Add async request Simple image gallery Edit color theme

--- app/src/main/java/com/example/tom/picwk/AboutActivity.java
+++ app/src/main/java/com/example/tom/picwk/AboutActivity.java
@@ -5,25 +5,77 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AboutActivity extends AppCompatActivity {
+
+    public int idx_image = 0;
+    public JSONArray image_list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        //String url = getIntent().getExtras().getString("url");
-        ImageView imgView = (ImageView)findViewById(R.id.imageView);
-        imgView.setImageURI(null);
-        //Picasso.get().load(url).into(imgView);
+        String url = getIntent().getExtras().getString("url");
+       // ArrayList<String> tags = getIntent().getExtras().getStringArrayList("tags");
+
+        try {
+            JSONArray tags_list = new JSONArray(getIntent().getExtras().getString("tags"));
+            String tags = "";
+            for (int i = 0; i < tags_list.length(); i++) {
+                tags += tags_list.getString(i) + " ";
+            }
+            TextView  txtView = (TextView) findViewById(R.id.textView4);
+            txtView.setText(tags);
+            image_list = new JSONArray(url);
+            ImageView imgView = (ImageView)findViewById(R.id.imageView);
+            imgView.setImageURI(null);
+            Picasso.get().load(image_list.getString(idx_image)).into(imgView);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 
-    public void back(View w) throws IOException {
+    public void back(View w) {
         Intent intent = new Intent(AboutActivity.this, MainActivity.class);
         startActivity(intent);
     }
+
+    public void prev(View w) {
+        ImageView imgView = (ImageView)findViewById(R.id.imageView);
+        if(idx_image > 0) {
+            try {
+                idx_image--;
+                Picasso.get().load(image_list.getString(idx_image)).into(imgView);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void next(View w) throws IOException {
+        ImageView imgView = (ImageView)findViewById(R.id.imageView);
+        if(idx_image < image_list.length() - 1) {
+            try {
+                idx_image++;
+                Picasso.get().load(image_list.getString(idx_image)).into(imgView);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/tom/picwk/MainActivity.java
+++ app/src/main/java/com/example/tom/picwk/MainActivity.java
@@ -1,231 +1,70 @@
 package com.example.tom.picwk;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Message;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.android.volley.RequestQueue;
+import com.loopj.android.http.*;
+import cz.msebera.android.httpclient.Header;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
-    RequestQueue requestQueue;
-    static final int INTERNET_REQ = 23;
-    static final String REQ_TAG = "VACTIVITY";
-    public static final String TAG_OK_HTTP_ACTIVITY = "OK_HTTP_ACTIVITY";
-
-    public OkHttpClient okHttpClient = null;
-
-    // Process child thread sent command to show server response text in activity main thread.
-   // private Handler displayRespTextHandler = null;
-
-    public static final int COMMAND_DISPLAY_SERVER_RESPONSE = 1;
-
-    public static final String KEY_SERVER_RESPONSE_OBJECT = "KEY_SERVER_RESPONSE_OBJECT";
-
-    public class CallAPI extends AsyncTask<String, String, String> {
-        public CallAPI(){
-            //set context variables if required
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            String urlString = params[0]; // URL to call
-            String data = params[1]; //data to post
-            OutputStream out = null;
-            String response = null;
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                out = new BufferedOutputStream(urlConnection.getOutputStream());
-                BufferedWriter writer = new BufferedWriter (new OutputStreamWriter(out, "UTF-8"));
-
-                writer.write(data);
-                writer.flush();
-                writer.close();
-                out.close();
-                urlConnection.connect();
-                int responseCode=urlConnection.getResponseCode();
-
-                if (responseCode == HttpsURLConnection.HTTP_OK) {
-                    String line;
-                    BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    while ((line=br.readLine()) != null) {
-                        response+=line;
-                    }
-                }
-                else {
-                    response="";
-
-                }
-
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            return response;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-
-    public String  performPostCall(String requestURL,
-                                   Pair<String, String> params) {
-
-        URL url;
-        String response = "";
-        try {
-            url = new URL(requestURL);
-
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(15000);
-            conn.setConnectTimeout(15000);
-            conn.setRequestMethod("POST");
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
-
-
-            OutputStream os = conn.getOutputStream();
-            BufferedWriter writer = new BufferedWriter(
-                    new OutputStreamWriter(os, "UTF-8"));
-            writer.write(params.first + '=' + params.second);
-
-            writer.flush();
-            writer.close();
-            os.close();
-            int responseCode=conn.getResponseCode();
-
-            if (responseCode == HttpsURLConnection.HTTP_OK) {
-                String line;
-                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                while ((line=br.readLine()) != null) {
-                    response+=line;
-                }
-            }
-            else {
-                response="";
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return response;
     }
 
     public void sends(View w) throws IOException, JSONException {
         EditText  txtEdit = (EditText) findViewById(R.id.editText);
+        TextView  txtView = (TextView) findViewById(R.id.textView);
 
-        //CallAPI task = new CallAPI();
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("text", txtEdit.getText().toString());
+        client.post("http://f255e43d.ngrok.io/execute", params, new TextHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String res) {
+                        // called when response HTTP status is "200 OK"
+                        System.out.println(res);
+                        try {
+                            JSONObject result = new JSONObject(res);
+                            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                            intent.putExtra("url",  result.getJSONArray("url").toString());
+                            intent.putExtra("tags", result.getJSONArray("tags").toString());
+                            //  intent.putExtra("tags", (Parcelable) result.getJSONArray("tags"));
+                            startActivity(intent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
 
-        //Pair<String, String> param = new Pair<>()
-        //String request = performPostCall("http://f8315884.ngrok.io/execute", new Pair<>("text", txtEdit.getText().toString()) );
-        //String request = task.doInBackground("http://f8315884.ngrok.io/execute", "text" + "=" + txtEdit.getText().toString());
-        String request = null;
-        JSONObject json = new JSONObject();
-
-        URL url = new URL("https://b9540b27.ngrok.io/execute");
-        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-        conn.setReadTimeout(10000);
-        conn.setConnectTimeout(15000);
-        conn.setRequestMethod("POST");
-        conn.setDoInput(true);
-        conn.setDoOutput(true);
-
-        List<Pair<String, String>> params = new ArrayList<>();
-        params.add(new Pair<>("text", txtEdit.getText().toString()));
-        OutputStream os = conn.getOutputStream();
-        BufferedWriter writer = new BufferedWriter(
-                new OutputStreamWriter(os, "UTF-8"));
-        writer.write(getQuery(params));
-        writer.flush();
-        writer.close();
-        os.close();
-
-        int responseCode=conn.getResponseCode();
-
-        if (responseCode == HttpsURLConnection.HTTP_OK) {
-            String line;
-            BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            while ((line=br.readLine()) != null) {
-                request+=line;
-            }
-        }
-        else {
-            request="";
-
-        }
-
-        //JSONObject obj = new JSONObject(request);
-
-        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
-        //intent.putExtra("url",  obj.getString("url"));
-        //intent.putExtra("tags", obj.getString("tags"));
-
-        startActivity(intent);
-    }
-
-    private String getQuery(List<Pair<String, String>> params) throws UnsupportedEncodingException
-    {
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
-
-        for (Pair<String, String> pair : params)
-        {
-            if (first)
-                first = false;
-            else
-                result.append("&");
-
-            result.append(URLEncoder.encode(pair.first, "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(pair.second, "UTF-8"));
-        }
-
-        return result.toString();
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    }
+                }
+        );
+        txtView.setText("loading...");
     }
 
 }
