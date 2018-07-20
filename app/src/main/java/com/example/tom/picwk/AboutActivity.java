f29f98fdf406ca2e2cd92515c221cf18e4457c5f
vanyarock01 <vanyarock01@gmail.com>
2018-07-20 18:43:04 +0300

    Add horizontal scroll gallery and fix app color theme

--- app/src/main/java/com/example/tom/picwk/AboutActivity.java
+++ app/src/main/java/com/example/tom/picwk/AboutActivity.java
@@ -1,12 +1,22 @@
 package com.example.tom.picwk;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.widget.AdapterView;
+import android.widget.Gallery;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.RequestCreator;
 
@@ -17,18 +27,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class AboutActivity extends AppCompatActivity {
+
+    private LinearLayout mGallery;
+    private LayoutInflater mInflater;
+    private HorizontalScrollView horizontalScrollView;
+    ImageView selectedImage;
 
     public int idx_image = 0;
     public JSONArray image_list;
     public List<RequestCreator> load_list = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(ProgressBar.VISIBLE);
         String url = getIntent().getExtras().getString("url");
-       // ArrayList<String> tags = getIntent().getExtras().getStringArrayList("tags");
 
         try {
             JSONArray tags_list = new JSONArray(getIntent().getExtras().getString("tags"));
@@ -39,19 +56,32 @@ public class AboutActivity extends AppCompatActivity {
             TextView  txtView = (TextView) findViewById(R.id.textView4);
             txtView.setText(tags);
             image_list = new JSONArray(url);
-            ImageView imgView = (ImageView)findViewById(R.id.imageView);
-            imgView.setImageURI(null);
             for (int i = 0; i < image_list.length(); i++) {
                 load_list.add(Picasso.get().load(image_list.getString(i)));
             }
 
-            load_list.get(idx_image).into(imgView);
+
+
+            LinearLayout layout = (LinearLayout) findViewById(R.id.linear);
+            for (int i = 0; i < load_list.size(); i++) {
+                ImageView imageView = new ImageView(this);
+                imageView.setId(i);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                layoutParams.gravity = Gravity.TOP | Gravity.END;
+                imageView.setLayoutParams(layoutParams);
+
+                imageView.setPadding(2, 2, 2, 2);
+                load_list.get(i).resize(1100, 600).centerInside().into(imageView);
+                layout.addView(imageView);
+            }
+
 
         } catch (JSONException e) {
             e.printStackTrace();
             Intent intent = new Intent(AboutActivity.this, MainActivity.class);
             startActivity(intent);
         }
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
     }
 
     public void back(View w) {
@@ -59,20 +89,5 @@ public class AboutActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void prev(View w) {
-        ImageView imgView = (ImageView)findViewById(R.id.imageView);
-        if(idx_image > 0) {
-            idx_image--;
-            load_list.get(idx_image).into(imgView);
-        }
-    }
-
-    public void next(View w) throws IOException {
-        ImageView imgView = (ImageView)findViewById(R.id.imageView);
-        if(idx_image < image_list.length() - 1) {
-            idx_image++;
-            load_list.get(idx_image).into(imgView);
-        }
-    }
 
 }
